df6dc11cd93efd6c06341f8b9f46bf93cf6a339c
kgostic <kategostic@gmail.com>
2022-07-26 15:28:13 -0700

    Added a test (passing) to catch this bug in the future.

--- tests/testthat/test-imprinting-probabilities.R
+++ tests/testthat/test-imprinting-probabilities.R
@@ -13,3 +13,9 @@ test_that("Range of years returned equals range of years passed.", {
   expect_equal(max(probs$birth_year), obs_year)
   expect_equal(min(probs$birth_year), min_year)
 })
+
+test_that("Observation year prior to 1996 returns known, valid probabilities", {
+  probs <- get_imprinting_probabilities(observation_years = 1919, countries = 'Aruba', df_format = 'long')
+  
+  expect_equal(probs$imprinting_prob, c(0.70, 0.91, 0.00, 0.00, 0.00, 0.00, 0.30, 0.09), tolerance = 0.01)
+})
