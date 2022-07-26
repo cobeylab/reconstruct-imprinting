4e2e8941341c556e87bc76f57b580753f530a144
kgostic <kategostic@gmail.com>
2022-07-26 08:44:53 -0700

    fixed function input order to resolve failed tests

--- tests/testthat/test-cocirculation-and-intensity.R
+++ tests/testthat/test-cocirculation-and-intensity.R
@@ -4,9 +4,9 @@ test_that("Observation year less than 13 years from birth year gives known-good 
   INTENSITY_DATA <- get_country_intensity_data("United States", 2010, min_specimens = 50)
 
   # Birth Year 2007
-  expect_equal(get_p_infection_year(2007, 2010, 0.28, 2010, INTENSITY_DATA), c(0.20244365, 0.18857213, 0.42628896, 0.03914203), tolerance = 0.000001)
+  expect_equal(get_p_infection_year(2007, 2010, INTENSITY_DATA, 2010, 0.28), c(0.20244365, 0.18857213, 0.42628896, 0.03914203), tolerance = 0.000001)
   # Birth Year 2010
-  expect_equal(get_p_infection_year(2010, 2010, 0.28, 2010, INTENSITY_DATA), c(0.2142477), tolerance = 0.000001)
+  expect_equal(get_p_infection_year(2010, 2010, INTENSITY_DATA, 2010, 0.28), c(0.2142477), tolerance = 0.000001)
 })
 
 
@@ -15,10 +15,10 @@ test_that("Observation year greater than 13 years from birth year gives known-go
   INTENSITY_DATA <- get_country_intensity_data("United States", 2010, min_specimens = 50)
 
   # Birth Year 1995
-  expect_equal(get_p_infection_year(1995, 2010, 0.28, 2010, INTENSITY_DATA), c(0.22467298, 0.24317721, 0.07694539, 0.03014844, 0.11330399, 0.05630441, 0.03586139, 0.04841809, 0.06950272, 0.01227025, 0.02001303, 0.01554628, 0.01089872), tolerance = 0.000001)
+  expect_equal(get_p_infection_year(1995, 2010, INTENSITY_DATA, 2010, 0.28), c(0.22467298, 0.24317721, 0.07694539, 0.03014844, 0.11330399, 0.05630441, 0.03586139, 0.04841809, 0.06950272, 0.01227025, 0.02001303, 0.01554628, 0.01089872), tolerance = 0.000001)
 
   # Birth Year 1998
-  expect_equal(get_p_infection_year(1998, 2010, 0.28, 2010, INTENSITY_DATA), c(0.066230545, 0.248907939, 0.123690388, 0.078780853, 0.106365603, 0.152684635, 0.026955481, 0.043964926, 0.034152297, 0.023942471, 0.022301922, 0.050416058, 0.004629224), tolerance = 0.000001)
+  expect_equal(get_p_infection_year(1998, 2010, INTENSITY_DATA, 2010, 0.28), c(0.066230545, 0.248907939, 0.123690388, 0.078780853, 0.106365603, 0.152684635, 0.026955481, 0.043964926, 0.034152297, 0.023942471, 0.022301922, 0.050416058, 0.004629224), tolerance = 0.000001)
 })
 
 test_that("Number of probabilities are same as number of years in input.", {
@@ -29,19 +29,19 @@ test_that("Number of probabilities are same as number of years in input.", {
   birth_year <- 1998
   obs_year <- 2010
 
-  expect_equal(length(get_p_infection_year(birth_year, obs_year, 0.28, obs_year, INTENSITY_DATA)), obs_year - birth_year + 1)
+  expect_equal(length(get_p_infection_year(birth_year, obs_year, INTENSITY_DATA, obs_year, 0.28)), obs_year - birth_year + 1)
 
   # Short span
   birth_year <- 2000
   obs_year <- 2003
 
-  expect_equal(length(get_p_infection_year(birth_year, obs_year, 0.28, obs_year, INTENSITY_DATA)), obs_year - birth_year + 1)
+  expect_equal(length(get_p_infection_year(birth_year, obs_year, INTENSITY_DATA, obs_year, 0.28)), obs_year - birth_year + 1)
 
   # One year
   birth_year <- 2008
   obs_year <- 2008
 
-  expect_equal(length(get_p_infection_year(birth_year, obs_year, 0.28, obs_year, INTENSITY_DATA)), obs_year - birth_year + 1)
+  expect_equal(length(get_p_infection_year(birth_year, obs_year, INTENSITY_DATA, obs_year, 0.28)), obs_year - birth_year + 1)
 })
 
 
